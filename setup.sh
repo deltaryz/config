8ded086a6ddfb5d53ebb24b6b3e7b10862dd18da
deltaryz <wincam97@gmail.com>
2020-12-02 00:16:41 -0600

    add todo

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ echo "Setup script for automated propagation of dotfiles"
 
 # TODO: Ask user to input what network interface to insert into getip (perhaps show an ifconfig output?)
 # TODO: Remove getip from /usr/bin and put it inside .tmux or something
+# TODO: Automatically install relevant packages & intelligently use the correct package manager/etc depending on the distro the script is running on
