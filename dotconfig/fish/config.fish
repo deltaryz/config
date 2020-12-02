5264f8054b09ff8898417c26434e9d5fde8034de
deltaryz <wincam97@gmail.com>
2020-12-02 00:04:18 -0600

    fix formatting

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -7,17 +7,17 @@ set distrostring (cat /etc/issue)
 
 # Alpine
 if string match -q "*Alpine*" $distrostring
-	alias apk="sudo apk"
-	alias rc-update="sudo rc-update"
-	alias gimme="sudo apk update && sudo apk add"
-	alias yeet="sudo apk del"
+    alias apk="sudo apk"
+    alias rc-update="sudo rc-update"
+    alias gimme="sudo apk update && sudo apk add"
+    alias yeet="sudo apk del"
 
 # Debian / Ubuntu
 else if string match -q "*Ubuntu*" $distrostring || string match -q "*Debian*" $distrostring
-	alias apt="sudo apt"
-	alias systemctl="sudo systemctl"
-	alias gimme="sudo apt update && sudo apt install"
-	alias yeet="sudo apt autoremove"
+    alias apt="sudo apt"
+    alias systemctl="sudo systemctl"
+    alias gimme="sudo apt update && sudo apt install"
+    alias yeet="sudo apt autoremove"
 
 # Arch / Manjaro
 else if string match -q "*Arch*" $distrostring || string match -q "*Manjaro*" $distrostring
@@ -51,17 +51,17 @@ end
 
 # Clean up terminal & prepare prompt
 function shell_init -d "Prepare shell/prompt for usage"
-	starship init fish | source
-	clear && neofetch
+    starship init fish | source
+    clear && neofetch
 end
 
 # Permanently add the given argument to PATH
 function addpath -d "Permanently adds the given argument to PATH"
-	set -U fish_user_paths $argv $fish_user_paths
+    set -U fish_user_paths $argv $fish_user_paths
 end
 
 # Set an environment variable permanently
 function addenvvar -d "Permanently sets an environment variable"
-	set -U $argv[1] $argv[2]
+    set -U $argv[1] $argv[2]
 end
 
