ab388450485504ec3765e6c70bec6a820ef80b21
deltaryz <wincam97@gmail.com>
2020-12-03 23:29:01 -0600

    add -y flag

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# FLAGS:
+# -y         skips confirmation prompt
+# --nogit    don't mess with git's config
+
 # init
 clear
 echo "-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --"
@@ -13,24 +17,30 @@ echo "* WARNING:"
 echo "This  script  will replace existing  dotfiles  and"
 echo "install new software on your system.  It will also"
 echo "set Git credentials and settings."
-# TODO: --nogit flag for something more friendly to people who aren't delta
-echo ""
-echo "Please confirm this potentially destructive action"
 echo ""
 
-# make user confirm
-while true; do
-    read -p "[y/N] > " yn
-    case $yn in
-        [Yy]* ) break;;
-        * ) exit;; # cancel script
-    esac
-done
+# check if we used -y flag
+if [ "$1" != "-y" ] && [ "$2" != "-y" ]; then
+    # flag not passed, make user confirm
+    echo "* (Use -y to skip this prompt)"
+    echo "Please confirm this potentially destructive action"
+    echo ""
+
+    while true; do
+        read -p "[y/N] > " yn
+        case $yn in
+            [Yy]* ) break;;
+            * ) exit;; # cancel script
+        esac
+    done
+fi
 
 echo ""
-echo ">>            Now starting setup!               <<"
+echo "-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --"
+echo ">>             Now starting setup!!             <<"
+echo "-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --"
 echo ""
-echo "* CONFIGURUING DOTFILES"
+echo "* CONFIGURING DOTFILES"
 echo ""
 
 # set file paths
@@ -66,9 +76,8 @@ rm -rf ~/.tmux/home
 
 echo ""
 echo "* CONFIGURING GIT"
-echo ""
 
-if [ "$1" != "--nogit" ] ; then
+if [ "$1" != "--nogit" ] && [ "$2" != "--nogit" ] ; then
     git config --global credential.helper store
     git config --global user.name "deltaryz"
     git config --global user.email "wincam97@gmail.com"
@@ -76,6 +85,12 @@ if [ "$1" != "--nogit" ] ; then
 else
     echo "(User passed --nogit flag, not touching git...)"
 fi
+
+echo ""
+echo "-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --"
+echo ">>     >>    ♥    Script is done    ♥    <<     <<"
+echo "-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --"
+echo ""
 
 # TODO: Ask user to input what network interface to insert into getip (perhaps show an ifconfig output?)
 # TODO: Automatically install relevant packages & intelligently use the correct package manager/etc depending on the distro the script is running on
